1e16e16e5df3b36a447dd322846ac470bcf2ad54
SleepingJempa <alirezaopmc@gmail.com>
2020-11-29 00:16:56 +0330

    Fix

--- e2/04 Primes Sum/solution.cpp
+++ e2/04 Primes Sum/solution.cpp
@@ -6,7 +6,7 @@ bool isPrime(int n) {
     if (n == 2) return true;
     if (n % 2 == 0) return false;
 
-    for(int i = 3; i * i <= n; i++) {
+    for(int i = 3; i * i <= n; i+=2) {
         if (n % i == 0) return false;
     }
 
